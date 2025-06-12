97fb98385479831f9627fffa8cb1bceaa2f73682
coinselor <coinselor@protonmail.com>
2025-06-12 22:48:36 +0100

    fix: help markdown

--- lib/help.sh
+++ lib/help.sh
@@ -13,34 +13,34 @@ ASCII
     gum style --foreground "242" --align "center" --width 61 "An Independent Script."
     
     local description_md="A deployment script to automate the setup and management of **Zenon Network** infrastructure."
-    gum format --type markdown "$description_md" | gum style --foreground "#808080" --align "center" --width 70 --margin "1 0"
+    gum format --type markdown "$description_md"
 
     gum style --border "rounded" --foreground "#A9A9A9" --border-foreground "#A9A9A9" --align "center" --width 61 --padding "0 1" --margin "1 0" "INTERACTIVE"
 
     gum style --foreground "#00FF00" "USAGE:"
     gum style --foreground "#00FF00" "sudo ./zenon.sh [COMMAND]"
     echo
-    gum style --foreground "#A9A9A9" --margin "0 0 1 0" "COMMAND can be 'zenon' (default) or 'hyperqube'."
+    gum format --type markdown "**COMMAND** can be 'zenon' (default) or 'hyperqube'."
     echo
 
-    gum style --foreground "#A9A9A9" "INTERACTIVE COMMANDS:"
+    gum style --foreground "#A9A9A9" "COMMANDS:"
     
     printf "%-12s %s\n" "zenon" "Show the interactive menu for Zenon Network." \
                          "hyperqube" "Show the interactive menu for HyperQube Network."
 
-    gum style --border "rounded" --foreground "#A9A9A9" --border-foreground "#A9A9A9" --align "center" --width 61 --padding "0 1" --margin "2 0 1 0" "NON-INTERACTIVE"
+    gum style --border "rounded" --foreground "#A9A9A9" --border-foreground "#A9A9A9" --align "center" --width 61 --padding "0 1" --margin "2 0 1 0" "COMMANDS"
 
     gum style --foreground "#00FF00" "USAGE:"
     gum style --foreground "#00FF00" "sudo ./zenon.sh [COMMAND] [ARGUMENTS]"
-    
+    echo
     local usage_details="**COMMAND** is a flag prefixed with '--' (e.g., --deploy)
 **TYPE** can be 'zenon' (default) or 'hyperqube'
 **REPOSITORY** is an **optional** HTTPS git URL
 **BRANCH** is an **optional** and case-sensitive git branch name"
     
     gum format --type markdown "$usage_details"
-
-    gum style --foreground "#A9A9A9" "NON-INTERACTIVE COMMANDS:"
+    echo
+    gum style --foreground "#A9A9A9" "COMMANDS:"
 
     printf "%-40s %s\n" "--deploy [type] [repository] [branch]" "Deploy a node." \
                                "--restore [type]" "Restore a node from the latest snapshot." \
