cdffabbfb8ef0c6819982b909f071bb652da9dec
coinselor <coinselor@protonmail.com>
2025-06-15 01:44:11 +0100

    fix: use -o flag in gum log

--- lib/logging.sh
+++ lib/logging.sh
@@ -2,7 +2,7 @@
 
 error_log() {
     if [[ "$ZNNSH_GUM_LOGS" == "true" ]]; then
-        gum log --time Kitchen --level error --prefix="Zenon.sh" "$@" 2> >(tee -a "$ZNNSH_LOG_FILE" >&2)
+        gum log --time Kitchen --level error --prefix="Zenon.sh" -o "$ZNNSH_LOG_FILE" "$@"
     else
         printf "[ERROR] %s\n" "$*" | tee -a "$ZNNSH_LOG_FILE" >&2
     fi
@@ -10,7 +10,7 @@ error_log() {
 
 warn_log() {
     if [[ "$ZNNSH_GUM_LOGS" == "true" ]]; then
-        gum log --time Kitchen --level warn --prefix="Zenon.sh" "$@" 2> >(tee -a "$ZNNSH_LOG_FILE" >&2)
+        gum log --time Kitchen --level warn --prefix="Zenon.sh" -o "$ZNNSH_LOG_FILE" "$@"
     else
         printf "[WARN] %s\n" "$*" | tee -a "$ZNNSH_LOG_FILE" >&2
     fi
@@ -18,14 +18,14 @@ warn_log() {
 
 info_log() {
     if [[ "$ZNNSH_GUM_LOGS" == "true" ]]; then
-        gum log --time Kitchen --level info --prefix="Zenon.sh" "$@" 2> >(tee -a "$ZNNSH_LOG_FILE" >&2)
+        gum log --time Kitchen --level info --prefix="Zenon.sh" -o "$ZNNSH_LOG_FILE" "$@"
     else
         printf "[INFO] %s\n" "$*" | tee -a "$ZNNSH_LOG_FILE"
     fi
 }
 success_log() {
     if [[ "$ZNNSH_GUM_LOGS" == "true" ]]; then
-        gum log --time Kitchen --level info --prefix="Zenon.sh" "✓ $*" 2> >(tee -a "$ZNNSH_LOG_FILE" >&2)
+        gum log --time Kitchen --level info --prefix="Zenon.sh" -o "$ZNNSH_LOG_FILE" "✓ $*"
     else
         printf "✓ %s\n" "$*" | tee -a "$ZNNSH_LOG_FILE"
     fi
